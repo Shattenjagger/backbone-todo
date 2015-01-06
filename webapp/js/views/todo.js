15a419d54f374460de19e30da040823c76f7f4e4
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-06 21:20:01 +0300

    Simple way

--- webapp/js/views/todo.js
+++ webapp/js/views/todo.js
@@ -7,6 +7,12 @@ define(
         'text!templates/todos.html'
     ],
     function ($, _, Backbone, Handlebars, todosTemplate) {
-        console.log("Another here");
+        var template = Handlebars.compile(todosTemplate);
+        console.log(template);
+        var context = {
+            done: true
+        };
+        var html = template(context);
+        console.log(html);
     }
 );
