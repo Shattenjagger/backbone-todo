c791f22187b983b2f7ef2a0a18d530860f1e341b
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-06 22:07:52 +0300

    Simple way

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -3,7 +3,7 @@ require.config({
     paths: {
         jquery: 'libs/jquery/jquery-2.1.3.min',
         underscore: 'libs/underscore/underscore-min',
-        handlebars: 'libs/handlebars/handlebars.runtime-v2.0.0',
+        handlebars: 'libs/handlebars/handlebars-v2.0.0',
         backbone: 'libs/backbone/backbone-min',
         text: 'libs/requirejs/text'
     },
