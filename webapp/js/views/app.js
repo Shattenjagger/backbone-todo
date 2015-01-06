9ef7db3368a1b6502609f3d0f7a6d3c84096c862
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-07 00:00:45 +0300

    Render functions

--- webapp/js/views/app.js
+++ webapp/js/views/app.js
@@ -23,17 +23,36 @@ define(
                 this.$todo_list = this.$('#todo-list');
 
                 this.Todos = new TodoCollection();
+                this.TodoFilter = '';
 
                 this.listenTo(this.Todos, 'add', this.addOne);
                 this.listenTo(this.Todos, 'reset', this.addAll);
-                //this.listenTo(this.Todos, 'change:completed', this.filterOne);
-                //this.listenTo(this.Todos, 'filter', this.filterAll);
-                //this.listenTo(this.Todos, 'all', this.render);
+                this.listenTo(this.Todos, 'change:completed', this.filterOne);
+                this.listenTo(this.Todos, 'filter', this.filterAll);
+                this.listenTo(this.Todos, 'all', this.render);
 
                 this.Todos.fetch();
             },
             render: function () {
-
+                var completed = this.Todos.completed();
+                var remaining = this.Todos.remaining();
+                if (this.Todos.length) {
+                    this.$main.show();
+                    this.$footer.show();
+                    this.$footer.html(this.template({
+                        completed: completed,
+                        remaining: remaining
+                    }));
+                    this.$('#filters li a')
+                        .removeClass('selected')
+                        .filter('[href="#/' + this.TodoFilter + '"]')
+                        .addClass('selected');
+                }
+                else {
+                    this.$main.hide();
+                    this.$footer.hide();
+                }
+                this.allCheckbox.checked = !remaining;
             },
             addOne: function (todo) {
                 var view = new TodoView({model: todo});
