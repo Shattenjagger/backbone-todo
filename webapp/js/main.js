bb7c814de061dcd4c3d860b445383d554bab5e80
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-06 23:32:52 +0300

    Refactored for TodoMVC

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -27,12 +27,6 @@ require.config({
     }
 });
 
-require(['collections/todos'], function (TodoList) {
-    var todo_list = new TodoList;
-    todo_list.create({ title: 'Hello, world!', completed: true });
-    console.log(todo_list);
-})
-
-//require(['views/app'], function (AppView) {
-//    var app_view = new AppView;
-//})
+require(['views/app'], function (AppView) {
+    var app_view = new AppView;
+});
--- webapp/js/views/app.js
+++ webapp/js/views/app.js
@@ -1,3 +1,22 @@
+define(
+    [
+        'backbone',
+        'handlebars',
+        'text!templates/stats.html'
+    ],
+    function (Backbone, Handlebars, statsTemplate) {
+        return Backbone.View.extend({
+            el: "#todoapp",
+            template: Handlebars.compile(statsTemplate),
+            initialize: function () {
+                this.allCheckbox = this.$('#toggle-all')[0];
+                this.$input = this.$('#new-todo');
+                this.$footer = this.$('#footer');
+                this.$main = this.$('#main');
+            }
+        });
+    }
+);
 /**
  * Created by guldan on 1/6/15.
  */
