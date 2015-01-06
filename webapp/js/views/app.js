c10143fdf366135ac8613c1781126274335e41fe
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-06 23:40:38 +0300

    First version

--- webapp/js/views/app.js
+++ webapp/js/views/app.js
@@ -2,9 +2,11 @@ define(
     [
         'backbone',
         'handlebars',
+        'views/todo',
+        'collections/todos',
         'text!templates/stats.html'
     ],
-    function (Backbone, Handlebars, statsTemplate) {
+    function (Backbone, Handlebars, TodoView, TodoCollection, statsTemplate) {
         return Backbone.View.extend({
             el: "#todoapp",
             template: Handlebars.compile(statsTemplate),
@@ -13,7 +15,22 @@ define(
                 this.$input = this.$('#new-todo');
                 this.$footer = this.$('#footer');
                 this.$main = this.$('#main');
+                this.$todo_list = this.$('#todo-list');
+
+                this.Todos = new TodoCollection();
+
+                this.listenTo(this.Todos, 'add', this.addOne);
+                this.listenTo(this.Todos, 'reset', this.addAll);
+            },
+            addOne: function (todo) {
+                var view = new TodoView({model: todo});
+                this.$todo_list.append(view.render().el);
+            },
+            addAll: function () {
+                this.$todo_list.html('');
+                this.Todos.each(this.addOne, this);
             }
+
         });
     }
 );
