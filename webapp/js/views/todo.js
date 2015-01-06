bee218d5789f991a6ef7d6435cfaee694743067d
Danila Balabaev <shattenjagger@yandex.ru>
2015-01-06 22:10:30 +0300

    Simple way

--- webapp/js/views/todo.js
+++ webapp/js/views/todo.js
@@ -7,17 +7,7 @@ define(
         'text!templates/todos.html'
     ],
     function ($, _, Backbone, Handlebars, todosTemplate) {
-        console.log("$: " + typeof $);
-        console.log("_: " + typeof _);
-        console.log("Backbone: " + typeof Backbone);
-        console.log("Handlebars: " + typeof Handlebars);
-
-        console.log(Handlebars);
-        var source = $("#test-template").html();
-        console.log(source);
-        //var template = Handlebars.compile(todosTemplate);
-        var template = Handlebars.compile(source);
-        console.log(template);
+        var template = Handlebars.compile(todosTemplate);
         var context = {
             'test-data': "Hello, world!"
         };
